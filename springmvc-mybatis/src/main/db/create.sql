de77076f6dab37ba4e79ea8d2104e4f0c8978ae8
zhangjinlin <zhangjinlin@xiaomi.com>
2017-11-27 20:21:27 +0800

    add springmvc-mybatis

---
+++ springmvc-mybatis/src/main/db/create.sql
@@ -0,0 +1,7 @@
+create table user
+(
+    id bigint auto_increment
+        primary key,
+    name varchar(50) null,
+    phone varchar(20) null
+);
---
+++ springmvc-mybatis/src/main/java/com/zhaoyh/service/UserService.java
@@ -0,0 +1,26 @@
+package com.zhaoyh.service;
+        import com.zhaoyh.model.User;
+        import com.zhaoyh.storage.UserStorage;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
+
+        import java.util.List;
+
+/**
+ * Created by zhaoyh on 2017/5/26.
+ */
+@Service
+public class UserService implements IUserService {
+
+    @Autowired
+    private UserStorage userStorage;
+
+    public void addUser(User user) {
+        userStorage.addUser(user.getName(), user.getPhone());
+    }
+
+    public List<User> getUserByPhone(String phone) {
+        return userStorage.queryUserByPhone(phone);
+    }
+
+}

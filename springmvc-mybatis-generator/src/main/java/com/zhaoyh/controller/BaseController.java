10b6fe59c51ce4175ecc52f6f431353a76e4e7b0
zhangjinlin <zhangjinlin@xiaomi.com>
2017-11-27 20:48:13 +0800

    增加mybatis-generator项目

---
+++ springmvc-mybatis-generator/src/main/java/com/zhaoyh/controller/BaseController.java
@@ -0,0 +1,26 @@
+package com.zhaoyh.controller;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhaoyh on 2017/5/26.
+ */
+public class BaseController {
+
+    /**
+     * 生成视图，用于继承
+     * @param view
+     */
+    protected ModelAndView innerCreateModelAndView (String view, Object... objects) {
+        assert (objects.length % 2) == 0;
+        Map<String, Object> modelMap = new HashMap<String, Object>();
+        for (int i = 0; i < objects.length; i++) {
+            String name = (String) objects[i++];
+            modelMap.put(name, objects[i]);
+        }
+        return new ModelAndView(view, modelMap);
+    }
+}
---
+++ springmvc-mybatis-generator/src/main/java/com/zhaoyh/service/IUserService.java
@@ -0,0 +1,15 @@
+package com.zhaoyh.service;
+
+import com.zhaoyh.model.User;
+
+import java.util.List;
+
+/**
+ * Created by zhaoyh on 2017/5/26.
+ */
+public interface IUserService {
+
+    void addUser(User user);
+
+    List<User> getUserByPhone(String phone);
+}
